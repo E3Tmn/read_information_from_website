786af85e13535e4179427b44cecdb2c77c0ee130
E3Tmn <nick1tapinyagin@yandex.ru>
2023-10-18 08:50:11 +0300

    add book titles

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from pathlib import Path
+from bs4 import BeautifulSoup
 
 
 def check_for_redirect(response):
@@ -7,18 +8,29 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
+def get_title_of_book(book_id):
+    url = f'https://tululu.org/b{book_id}/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find('head').find('title')
+    title_text = title_tag.text.split('-')
+    name_of_book = title_text[0].strip()
+    return name_of_book
+
+
 def main():
     for count in range(10):
+        book_id = f'3216{count}'
         payloads = {
-            'id': f'3216{count}'
+            'id': book_id
         }
         url = f"https://tululu.org/txt.php"
         try:
             response = requests.get(url,params=payloads)
             response.raise_for_status()
             check_for_redirect(response)
+            filename = get_title_of_book(book_id)
             Path('books').mkdir(exist_ok=True)
-            filename = f'text{count}.txt'
             with open(f'books/{filename}', 'wb') as file:
                 file.write(response.content)
         except requests.HTTPError:
