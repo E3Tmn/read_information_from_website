eb4060ba84c0c548ba905a69af719989bc506727
E3Tmn <nick1tapinyagin@yandex.ru>
2023-10-18 06:47:06 +0300

    add check_for_redirect

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ import requests
 from pathlib import Path
 
 
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
+
+
 def main():
     for count in range(10):
-        url = f"https://tululu.org/txt.php?id=3216{count}"
-        response = requests.get(url)
-        response.raise_for_status()
-        Path('books').mkdir(exist_ok=True)
-        filename = f'text{count}.txt'
-        with open(f'books/{filename}', 'wb') as file:
-            file.write(response.content)
+        payloads = {
+            'id': f'3216{count}'
+        }
+        url = f"https://tululu.org/txt.php"
+        try:
+            response = requests.get(url,params=payloads)
+            response.raise_for_status()
+            check_for_redirect(response)
+            Path('books').mkdir(exist_ok=True)
+            filename = f'text{count}.txt'
+            with open(f'books/{filename}', 'wb') as file:
+                file.write(response.content)
+        except requests.HTTPError:
+            print(requests.HTTPError)
 
 
 if __name__ == "__main__":
