706c3790a1c8dbca8d00d46dd1401d6d7c107dc3
E3Tmn <nick1tapinyagin@yandex.ru>
2023-10-23 11:32:40 +0300

    add function 'download_picture'

--- main.py
+++ main.py
@@ -3,7 +3,21 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 import os
 from pathvalidate import sanitize_filename
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
+
+
+def download_picture(count, soup):
+    picture_url = soup.find('div', class_='bookimage').find('a').find('img')['src']
+    print(os.path.splitext(picture_url))
+    url = urljoin('https://tululu.org', picture_url)
+    path = urlparse(url).path
+    ext = os.path.splitext(path)[1]
+    response = requests.get(url)
+    response.raise_for_status()
+    image_folder = 'image'
+    Path(image_folder).mkdir(exist_ok=True)
+    with open(os.path.join(image_folder, f'{count}{ext}'), 'wb') as file:
+        file.write(response.content)
 
 
 def check_for_redirect(response):
@@ -11,14 +25,9 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def get_title_of_book(book_id):
-    url = f'https://tululu.org/b{book_id}/'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
+def get_title_of_book(soup):
     title_tag = soup.find('head').find('title')
     title_text = title_tag.text.split('- ')
-    picture_url = soup.find('div', class_='bookimage').find('a').find('img')['src']
-    print(urljoin('https://tululu.org', picture_url))
     name_of_book = title_text[0].strip()
     return f"{sanitize_filename(name_of_book)}.txt"
 
@@ -29,18 +38,22 @@ def main():
         payloads = {
             'id': book_id
         }
-        url = f"https://tululu.org/txt.php"
+        book_url = f"https://tululu.org/txt.php"
         try:
-            response = requests.get(url, params=payloads)
+            response = requests.get(book_url, params=payloads)
             response.raise_for_status()
             check_for_redirect(response)
-            filename = get_title_of_book(book_id)
-            folder = 'books'
-            Path(folder).mkdir(exist_ok=True)
-            with open(os.path.join(folder, f'{count}.{filename}'), 'wb') as file:
+            title_page_url = f'https://tululu.org/b{book_id}/'
+            title_page_response = requests.get(title_page_url)
+            soup = BeautifulSoup(title_page_response.text, 'lxml')
+            filename = get_title_of_book(soup)
+            download_picture(count, soup)
+            book_folder = 'books'
+            Path(book_folder).mkdir(exist_ok=True)
+            with open(os.path.join(book_folder, f'{count}.{filename}'), 'wb') as file:
                 file.write(response.content)
         except requests.HTTPError:
-            print(requests.HTTPError)
+            pass
 
 
 if __name__ == "__main__":
